6b61aa8558f119c7d084ea6ea16165cae4e20245
Manu <mail@manuelakunst.de>
2019-12-18 12:42:33 +0100

    Getting User Data of logged in User via DataAccess Library

---
+++ TRMDataManager.Library/DataAccess/UserData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TRMDataManager.Library.Internal.DataAccess;
+using TRMDataManager.Library.Internal.Models;
+
+namespace TRMDataManager.Library.DataAccess
+{
+    public class UserData
+    {
+        public List<UserModel> GetUserById(string id)
+        {
+            var sql = new SqlDataAccess();
+
+            // anonyme Klasseninstanz, um den Parameter id uebergeben zu koennen
+            var p = new { Id = id };
+
+            var output = sql.LoadData<UserModel, dynamic>("dbo.spUserLookup", p, "TRMData");
+            return output;
+        }
+    }
+}
---
+++ TRMDataManager/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using TRMDataManager.Library.DataAccess;
+using TRMDataManager.Library.Internal.Models;
+
+namespace TRMDataManager.Controllers
+{
+    [Authorize]
+    public class UserController : ApiController
+    {
+        public List<UserModel> GetById()
+        {
+            var userId = RequestContext.Principal.Identity.GetUserId();
+            var data = new UserData();
+            return data.GetUserById(userId);
+        }
+
+    }
+}
