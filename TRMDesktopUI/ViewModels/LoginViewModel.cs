2d4c4eb5a79c6638060e457770231417fbcd73cc
Manu <mail@manuelakunst.de>
2019-12-20 15:03:24 +0100

    EventAggregation
    Publishing a LogIn Event on LoginView and ShellView as a subscriber changes its control from LoginView to SalesView

--- TRMDesktopUI/ViewModels/LoginViewModel.cs
+++ TRMDesktopUI/ViewModels/LoginViewModel.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TRMDesktopUI.EventModels;
 using TRMDesktopUI.Helpers;
 using TRMDesktopUI.Library.Api;
 
@@ -14,10 +15,12 @@ namespace TRMDesktopUI.ViewModels
 		private string _userName;
 		private string _password;
 		private IAPIHelper _apiHelper;
+		private IEventAggregator _events;
 
-		public LoginViewModel(IAPIHelper apiHelper)
+		public LoginViewModel(IAPIHelper apiHelper, IEventAggregator events)
 		{
 			_apiHelper = apiHelper;
+			_events = events;
 		}
 
 		public string UserName
@@ -98,8 +101,7 @@ namespace TRMDesktopUI.ViewModels
 				// capture more information about the user
 				await _apiHelper.GetLoggedInUserInfo(result.Access_Token);
 
-				// nur für mich um zu sehen, ob es funktioniert
-				Console.WriteLine((_apiHelper as APIHelper).liu.FirstName);
+				_events.PublishOnUIThread(new LogOnEvent());
 			}
 			catch (Exception ex)
 			{
--- TRMDesktopUI/ViewModels/ShellViewModel.cs
+++ TRMDesktopUI/ViewModels/ShellViewModel.cs
@@ -4,17 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Caliburn.Micro;
+using TRMDesktopUI.EventModels;
 
 namespace TRMDesktopUI.ViewModels
 {
-    public class ShellViewModel : Conductor<object>
+    public class ShellViewModel : Conductor<object>, IHandle<LogOnEvent>
     {
-        private LoginViewModel _loginVM;
+        private SalesViewModel _salesVM;
+        private IEventAggregator _events;
+        private SimpleContainer _container;
 
-        public ShellViewModel(LoginViewModel loginVM)
+        public ShellViewModel(SalesViewModel salesVM, IEventAggregator events, SimpleContainer container)
+        {            
+            _salesVM = salesVM;
+            _container = container;
+            _events = events;
+
+            _events.Subscribe(this);
+
+            ActivateItem(_container.GetInstance<LoginViewModel>());
+        }
+
+        public void Handle(LogOnEvent message)
         {
-            _loginVM = loginVM;
-            ActivateItem(_loginVM);
+            ActivateItem(_salesVM);
         }
     }
 }
