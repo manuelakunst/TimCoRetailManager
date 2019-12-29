b5c418ff404a14ce20d698f702519ccc7082cad8
Manu <mail@manuelakunst.de>
2019-12-29 23:47:27 +0100

    Displaying Users and Roles of Users

--- TRMDesktopUI/ViewModels/ShellViewModel.cs
+++ TRMDesktopUI/ViewModels/ShellViewModel.cs
@@ -41,6 +41,11 @@ namespace TRMDesktopUI.ViewModels
             TryClose();
         }
 
+        public void UserManagement()
+        {
+            ActivateItem(IoC.Get<UserDisplayViewModel>());
+        }
+
         public void LogOut()
         {
             _user.ResetUser();
---
+++ TRMDesktopUI/ViewModels/UserDisplayViewModel.cs
@@ -0,0 +1,78 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using TRMDesktopUI.Library.Api;
+using TRMDesktopUI.Library.Models;
+
+namespace TRMDesktopUI.ViewModels
+{
+    public class UserDisplayViewModel : Screen
+    {
+		private readonly IWindowManager _window;
+		private readonly IUserEndpoint _userEndpoint;
+		private readonly StatusInfoViewModel _status;
+
+		private BindingList<UserModel> _users;
+
+		public BindingList<UserModel> Users
+		{
+			get { return _users; }
+			set 
+			{
+				_users = value;
+				NotifyOfPropertyChange(() => Users);
+			}
+		}
+
+
+		public UserDisplayViewModel(IUserEndpoint userEndpoint, IWindowManager window, StatusInfoViewModel status)
+		{
+			_userEndpoint = userEndpoint;
+			_window = window;
+			_status = status;
+
+		}
+
+		protected override async void OnViewLoaded(object view)
+		{
+			base.OnViewLoaded(view);
+			try
+			{
+				await LoadUsers();
+			}
+			catch (Exception ex)
+			{
+				dynamic settings = new ExpandoObject();
+				settings.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+				settings.ResizeMode = ResizeMode.NoResize;
+				settings.Title = "System Error";
+
+				if (ex.Message.Equals("Unauthorized"))
+				{
+					_status.UpdateMessage("Unauthorized Access", "You do not have permission to interact with the Sales Form.");
+					_window.ShowDialog(_status, null, settings);
+				}
+				else
+				{
+					_status.UpdateMessage("Fatal Exception", ex.Message);
+					_window.ShowDialog(_status, null, settings);
+				}
+
+				TryClose();
+			}
+		}
+
+		private async Task LoadUsers()
+		{
+			var userList = await _userEndpoint.GetUsers();
+			Users = new BindingList<UserModel>(userList);
+		}
+
+	}
+}
